889ba320ef7be4a0317bad9c8c65dc91266997de
nbonfils <nils.bonfils@gmail.com>
2021-12-02 13:46:10 +0100

    Clean day 2

--- 02.lisp
+++ 02.lisp
@@ -1,5 +1,31 @@
 (defvar input (mapcar #'uiop:split-string (uiop:read-file-lines "02.txt")))
 
+(defun deltas ()
+  (loop for i in input
+        for horizontal = (if (equal (first i) "forward") (parse-integer (second i)) 0)
+        for vertical = (cond ((equal (first i) "down") (parse-integer (second i)))
+                             ((equal (first i) "up") (- (parse-integer (second i))))
+                             (t 0))
+        collect (list horizontal vertical)))
+
+(defun part1 ()
+  (apply #'* (reduce (lambda (acc i)
+                       (list (+ (first acc) (first i))
+                             (+ (second acc) (second i))))
+                     (deltas))))
+
+(defun part2 ()
+  (apply #'* (reduce (let ((aim 0))
+                       (lambda (acc i)
+                         (incf aim (second i))
+                         (list (+ (first acc) (first i))
+                               (+ (second acc) (* aim (first i))))))
+                     (deltas))))
+
+(format t "Part 1: ~a~&Part 2: ~a~&" (part1) (part2))
+
+;;; Initial raw version
+
 (loop with horizontal-tot = 0
       with depth-tot = 0
       for i in input
